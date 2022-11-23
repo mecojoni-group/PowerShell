9c139c6c4af0037a20b9a36ce4516fb8d7cd9e8b
edoardo beltramo <edoardo.beltramo@outlook.it>
2022-11-23 21:39:38 +0100

    Move scripts to the root folder

---
+++ SetBulkAliasToUserMailboxes.ps1
@@ -0,0 +1,16 @@
+﻿# Connect to MS365 with 2FA
+Connect-ExchangeOnline -UserPrincipalName "a.e.beltramo@cairocommunication.it" -ShowProgress $true
+
+# Fetch the list of mailboxes filtering by needed fields
+$MailboxesEditore = Get-MailBox | where {$_.emailAddresses -like "*@cairoeditore.it" } | where {$_.RecipientTypeDetails -like "UserMailbox" } | Get-User | Select Firstname, Lastname, UserPrincipalName
+
+# Set aliases for each mailbox
+foreach($MailboxEditore in $MailboxesEditore)
+{
+    $Name = $MailboxEditore.Firstname.ToLower().replace(' ','')
+    $Lastname = $MailboxEditore.Lastname.ToLower().replace(' ','')
+    $Domain = "@cairoeditore.it"
+    $Mail = $MailboxEditore.UserPrincipalName
+    $Alias = $Name + $Lastname + $Domain
+    Set-Mailbox $Mail -EmailAddresses @{Add="$Alias"}
+}
